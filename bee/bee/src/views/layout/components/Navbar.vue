9158f3771c7e3c6b00e1eaaab2fd30a80e8b26ba
peng.huang <peng.huang@future-data.cn>
2018-01-17 18:10:12 +0800

    修改eslint语法问题

--- bee/bee/src/views/layout/components/Navbar.vue
+++ bee/bee/src/views/layout/components/Navbar.vue
@@ -41,7 +41,7 @@ export default {
       removeToken()
       this.$router.push({ path: '/login' })
       this.$store.dispatch('LogOut').then(() => {
-        location.reload();  // 为了重新实例化vue-router对象 避免bug
+        location.reload()//  为了重新实例化vue-router对象 避免bug
       })
     }
   }
